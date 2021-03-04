9aabc36a9fd771fbe10291d60db58f5978f59f54
João Santos <jonhy81@gmail.com>
2021-03-04 14:31:42 +0000

    DQA-0: test new Package QA.

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -40,7 +40,7 @@ abstract class AbstractTest extends TestCase
     protected function assertContainsNotContains($content, array $expected)
     {
         if (!empty($expected['contains'])) {
-            $this->assertContains($this->trimEachLine($expected['contains']), $this->trimEachLine($content));
+            $this->assertContains($this->trimEachLine($expected['contains']), [$this->trimEachLine($content)]);
             $this->assertEquals(substr_count($this->trimEachLine($content), $this->trimEachLine($expected['contains'])), 1, 'String found more than once.');
         }
 
